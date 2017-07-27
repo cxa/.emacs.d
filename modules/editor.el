ed0e08041113563856ffd440cfa0f1ed6df37ec4
CHEN Xian-an <xianan.chen@gmail.com>
2017-07-27 10:38:54 +0800

    Global auto revert

--- modules/editor.el
+++ modules/editor.el
@@ -74,9 +74,13 @@
   (setq x-super-keysym 'meta))
 
 ;; Backups
-(setq backup-by-copying t
-      backup-directory-alist '(("." . "~/.emacs-saves"))
-      delete-old-versions t
-      kept-new-versions 6
-      kept-old-versions 2
-      version-control t)
+(setq make-backup-files nil)
+;; (setq backup-by-copying t
+;;       backup-directory-alist '(("." . "~/.emacs-saves"))
+;;       delete-old-versions t
+;;       kept-new-versions 6
+;;       kept-old-versions 2
+;;       version-control t)
+
+;;
+(global-auto-revert-mode t)
