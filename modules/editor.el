490a1c170d8ca5a231f0d45dbe65b446e3d350cc
CHEN Xian-an <xianan.chen@gmail.com>
2017-07-28 01:46:54 +0800

    Indent region and buffer

--- modules/editor.el
+++ modules/editor.el
@@ -91,3 +91,21 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
+;; Indent
+(defun indent-buffer ()
+  "Indent the currently visited buffer."
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun indent-region-or-buffer ()
+  "Indent a region if selected, otherwise the whole buffer."
+  (interactive)
+  (save-excursion
+    (if (region-active-p)
+        (progn
+          (indent-region (region-beginning) (region-end))
+          (message "Indented selected region."))
+      (progn
+        (indent-buffer)
+        (message "Indented buffer.")))))
+(global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
