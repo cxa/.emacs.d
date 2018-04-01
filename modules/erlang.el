6a0c275c04dc46341659d38839cdefb5053a5fe1
CHEN Xian-an <xianan.chen@gmail.com>
2018-04-01 18:49:51 +0800

    Erlang: export function shortcut

--- modules/erlang.el
+++ modules/erlang.el
@@ -33,10 +33,57 @@
   ((warning line-start (file-name) ":" line ": Warning:" (message) line-end)
    (error line-start (file-name) ":" line ": " (message) line-end)))
 
+;; https://github.com/bodil/ohai-emacs/blob/master/modules/ohai-erlang.el
+(defun stripwhite (str)
+  "Remove any whitespace from STR."
+  (let ((s (if (symbolp str) (symbol-name str) str)))
+    (replace-regexp-in-string "[ \t\n]*" "" s)))
+
+(defun erlang-forward-arg ()
+  (forward-sexp))
+
+;; Find the name of the function under the cursor.
+(defun erlang-current-function ()
+  (save-excursion
+    (if (not (equal (point) (point-max))) (forward-char))
+    (erlang-beginning-of-function)
+    (let ((beg (point))
+          (fun-name)
+          (fun-arity)
+          (result '()))
+      (search-forward "(")
+      (backward-char)
+      (setq fun-name (stripwhite (buffer-substring beg (point))))
+      (if (char-equal (char-after) ?\))
+          (setq fun-arity 0)
+        (forward-char)
+        (setq fun-arity 0)
+        (while (not (char-equal (char-after) ?\)))
+          (erlang-forward-arg)
+          (setq fun-arity (+ fun-arity 1))))
+      (format "%s/%d" fun-name fun-arity))))
+
+(defun erlang-move-to-export-insertion ()
+  (interactive)
+    (goto-char (point-max))
+    (if (search-backward-regexp "^-export" 0 t)
+    (end-of-line)
+      (search-backward-regexp "^-" 0 t)
+      (end-of-line)))
+
+(defun erlang-export (fun-arity)
+  (interactive
+   (list (read-no-blanks-input "function/arity: " (erlang-current-function))))
+  (save-excursion
+    (erlang-move-to-export-insertion)
+    (newline)
+    (insert (format "-export ([%s])." fun-arity))))
+
 (add-hook 'erlang-mode-hook
           (lambda ()
             (flycheck-select-checker 'erlang-otp)
-            (flycheck-mode)))
+            (flycheck-mode)
+            (local-set-key "\C-ce" 'erlang-export)))
 ;; prevent annoying hang-on-compile
 (defvar inferior-erlang-prompt-timeout t)
 ;; default node name to emacs@localhost
