b5fad488e2f7a1cccbc150f027714992ba1bb2ab
CHEN Xian-an <xianan.chen@gmail.com>
2018-09-25 01:35:51 +0800

    Add company backend for docker instructions

--- modules/docker.el
+++ modules/docker.el
@@ -1,2 +1,24 @@
 (require 'dockerfile-mode)
-(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+(require 'company)
+
+(defconst docker-instructions
+  '("FROM " "MAINTAINER " "RUN " "CMD " "EXPOSE " "ENV " "ARG "
+    "ADD " "COPY " "ENTRYPOINT " "VOLUME " "USER " "WORKDIR " "ONBUILD "
+    "LABEL " "STOPSIGNAL " "SHELL " "HEALTHCHECK "))
+
+(defun docker-instructions-company-backend (command &optional arg &rest ignored)
+  (interactive (list 'interactive))
+  (cl-case command
+    (interactive (company-begin-backend 'docker-instructions-company-backend))
+    (prefix (and (eq major-mode 'dockerfile-mode)
+                 (company-grab-line "^[a-zA-Z|/]+$")))
+    (candidates
+     (if (string-equal "/" arg) docker-instructions ; use / to list all
+       (seq-filter (lambda (c) (string-prefix-p arg c t)) docker-instructions)))
+    (sorted t)
+    (no-cache t)))
+
+(add-hook
+ 'dockerfile-mode-hook
+ (lambda ()
+   (add-to-list 'company-backends 'docker-instructions-company-backend)))
